b25ba20dff6a37776d2883055bad665baa863d51
Nguyenquyen171 <138420361+Nguyenquyen171@users.noreply.github.com>
2023-09-07 16:52:00 +0700

    Quyen nop bai tap

--- C0732G1_NguyenQuyen_Modul2/src/ss3/baitap/DiagonalLine_Array_Execise7.java
+++ C0732G1_NguyenQuyen_Modul2/src/ss3/baitap/DiagonalLine_Array_Execise7.java
@@ -6,11 +6,11 @@ public class DiagonalLine_Array_Execise7 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int length;
-        int[][] array;
+        double[][] array;
         do {
             System.out.println("Enter the length of the square matrix: ");
             length = scanner.nextInt();
-            array = new int[length][length];
+            array = new double[length][length];
             if (length > 0 && length <= 20 ) {
                 for (int i = 0; i < length; i++) {
                     for (int j = 0; j < length; j++) {
@@ -27,11 +27,11 @@ public class DiagonalLine_Array_Execise7 {
         } while (true);
         for (int i = 0; i < length; i++) {
             for (int j = 0; j < length; j++) {
-                System.out.print(array[i][j]);
+                System.out.print(array[i][j] + "  ");
             }
             System.out.println();
         }
-        int sum = 0;
+        double sum = 0;
         for (int i = 0; i < array.length; i++) {
             sum += array[i][array.length - 1 - i];
         }
