263c90f6a38b7ad59377d2c2a88af2b605215d65
Nguyenquyen171 <138420361+Nguyenquyen171@users.noreply.github.com>
2023-09-07 16:09:12 +0700

    Quyen nop bai tap

--- C0732G1_NguyenQuyen_Modul2/src/ss3/baitap/Element_Exercise1.java
+++ C0732G1_NguyenQuyen_Modul2/src/ss3/baitap/Element_Exercise1.java
@@ -15,34 +15,44 @@ public class Element_Exercise1 {
                 System.out.println("Size does not exceed 20");
             }
         } while (size > 20);
-        array= new int[size];
+        array = new int[size];
         for (int i = 0; i < size; i++) {
-            System.out.println("Enter element "+ (i+1)+ " :");
-            array[i]= scanner.nextInt();
+            System.out.println("Enter element " + (i + 1) + " :");
+            array[i] = scanner.nextInt();
         }
 
-        System.out.printf( "Elements in array: ");
+        System.out.printf("Elements in array: ");
         for (int j = 0; j < array.length; j++) {
-            System.out.print(array[j]+ " ");
+            System.out.print(array[j] + " ");
         }
         System.out.println();
         System.out.println("Enter the element to delete: ");
-        int deleteNumber= scanner.nextInt();
-        boolean check=false;
+        int deleteNumber = scanner.nextInt();
+        boolean check = false;
+        String index= "";
         for (int i = 0; i < array.length; i++) {
             if (array[i] == deleteNumber) {
-                check=true;
+                index = index+ i + " ";
+            }
+
+        }
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] == deleteNumber) {
+                check = true;
                 for (int j = i; j < array.length - 1; j++) {
                     array[j] = array[j + 1];
                 }
-                array[array.length - 1] = 0;
-                i--;
+                if ( array[array.length - 1] !=0) {
+                    array[array.length - 1] = 0;
+                    i--;
+                }
 
             }
         }
-        if (check){
+        if (check) {
             System.out.println("The new array after deleting the duplicate element is:" + Arrays.toString(array));
-        }else {
+            System.out.println("Delete position of array " + index);
+        } else {
             System.out.println("The number you want to delete is in the element" + deleteNumber);
         }
     }
