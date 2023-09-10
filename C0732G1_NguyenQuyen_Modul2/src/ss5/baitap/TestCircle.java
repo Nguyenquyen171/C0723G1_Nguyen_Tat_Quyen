af3fca232beb27d5b307b8b45dbdd2b359a7b7f8
Nguyenquyen171 <138420361+Nguyenquyen171@users.noreply.github.com>
2023-09-10 18:12:06 +0700

    Quyen nop bai tap

---
+++ C0732G1_NguyenQuyen_Modul2/src/ss5/baitap/TestCircle.java
@@ -0,0 +1,37 @@
+package ss5.baitap;
+
+import java.util.Scanner;
+
+public class TestCircle {
+    public static void main(String[] args) {
+        Circle circle = new Circle();
+        System.out.println(circle.getArea());
+        System.out.println(circle.getRadius());
+
+    }
+
+    public static class Circle {
+        private double radius = 1.0;
+        private String color = "red";
+
+        public double getRadius() {
+            return radius;
+        }
+
+        public String getColor() {
+            return color;
+        }
+
+        public void setRadius(double radius) {
+            this.radius = radius;
+        }
+
+        public Circle() {
+        }
+
+        public double getArea() {
+            return 3.14 * radius * radius;
+        }
+
+    }
+}
