59d4ad49342d6829c533678d481d050a8c0deab6
Nguyenquyen171 <138420361+Nguyenquyen171@users.noreply.github.com>
2023-09-14 14:02:47 +0700

    Quyen nop bai tap

--- C0732G1_NguyenQuyen_Modul2/src/ss8/Exercise/Tennis.java
+++ C0732G1_NguyenQuyen_Modul2/src/ss8/Exercise/Tennis.java
@@ -1,12 +1,15 @@
 package ss8.Exercise;
 
 public class Tennis {
-   public static final String SCORE = "Love";
-    public static  final String SCORE1 = "Fifteen";
+    public static final String SCORE = "Love";
+    public static final String SCORE1 = "Fifteen";
     public static final String SCORE2 = "Thirty";
     public static final String SCORE3 = "Forty";
-    public static String drawDisplay(int playerScore){
+
+    public static String drawDisplay(int playerScore) {
+
         String resultDisplay;
+
         switch (playerScore) {
             case 0:
                 resultDisplay = "Love-All";
@@ -26,31 +29,36 @@ public class Tennis {
         }
         return resultDisplay;
     }
-    public static String winDisplay(int player1Score, int player2Score){
 
-
+    public static String winDisplay(int player1Score, int player2Score) {
+        if (player1Score == player2Score) return drawDisplay(player1Score);
         int minusResult = player1Score - player2Score;
-
         if (minusResult == 1) return "Advantage player1";
         if (minusResult == -1) return "Advantage player2";
         if (minusResult >= 2) return "Win for player1";
         return "Win for player2";
+
     }
-    public static String matchDisplay(int playerScore){
+
+    public static String matchDisplay(int playerScore) {
         String resultScore = "";
-        switch(playerScore) {
+        switch (playerScore) {
             case 0:
-                resultScore = SCORE ;
+                resultScore += SCORE;
                 break;
             case 1:
-                resultScore = SCORE1;
+                resultScore += SCORE1;
                 break;
             case 2:
-                resultScore = SCORE2;
+                resultScore += SCORE2;
                 break;
             case 3:
-                resultScore = SCORE3;
+                resultScore += SCORE3;
         }
         return resultScore;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(winDisplay(4,4));
     }
 }
