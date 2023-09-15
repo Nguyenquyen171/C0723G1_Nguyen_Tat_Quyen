2e80619922c1f3eb7b2aa9d1c0a3ebd4a95bd4ac
Nguyenquyen171 <138420361+Nguyenquyen171@users.noreply.github.com>
2023-09-15 15:45:37 +0700

    Quyen nop bai tap

--- C0732G1_NguyenQuyen_Modul2/src/ss10/Exercise/MyList.java
+++ C0732G1_NguyenQuyen_Modul2/src/ss10/Exercise/MyList.java
@@ -1,16 +1,88 @@
 package ss10.Exercise;
+import java.util.Arrays;
+
+//import static com.sun.tools.javac.util.ArrayUtils.ensureCapacity;
 
 public class MyList<E> {
-        private int size;
-        private static final int DEFAULT_CAPACITY = 10;
-        private Object elements[];
-        public MyList(){
+    private int size = 0;
+    private static final int DEFAULT_CAPACITY = 10;
+    private Object elements[];
 
-        }
-        public MyList(int capacity){
-
-        }
-
+    public MyList() {
+        elements = new Object[DEFAULT_CAPACITY];
     }
+
+    public MyList(int capacity) {
+        elements = new Object[capacity];
+    }
+
+    public void add(int index, E element) {
+        if (index> size || index <0) {
+            throw new ArithmeticException("The index: " + index + " out of bound" );
+        } else {
+            if (size == elements.length) {
+                ensureCapacity(size + 1);
+            }
+            for (int i = size - 1; i >= index; i--) {
+                elements[i + 1] = elements[i];
+            }
+            elements[index] = element;
+            size++;
+        }
+    }
+    public E remove(int index){
+        if (index < 0 || index >= size) {
+            throw new ArithmeticException("The index: " + index + " out of bound");
+        } else {
+            for (int i = index; i < size; i++) {
+                elements[i] = elements[i + 1];
+            }
+            elements[size - 1] = null;
+
+        }
+        size--;
+        return (E) elements[index];
+    }
+    public void ensureCapacity(int minCapacity) {
+        if (minCapacity > elements.length) {
+            Object[] newElements = new Object[elements.length * 2];
+            for (int i = 0; i < size; i++) {
+                newElements[i] = elements[i];
+            }
+            elements = newElements;
+        }
+    }
+    public  String toString(){
+        StringBuilder result = new StringBuilder("[");
+        for (int i = 0; i < size; i++) {
+            result.append(elements[i]);
+            if (i<size-1){
+                result.append(", ");
+            }
+        }
+        return result.toString() + "]";
+    }
+    public Object clone(){
+        Object[] newArr = new Object[size];
+        for (int i = 0; i < size; i++) {
+            newArr[i] = elements[i];
+        }
+        return newArr;
+    }
+    public void clear (){
+        for (int i = 0; i < size; i++) {
+            elements[i] = null;
+        }
+        size = 0;
+    }
+    boolean isEmpty(){
+        if(size!=0){
+            return false;
+        }
+        return true;
+    }
+
+
+}
 
 
---
+++ C0732G1_NguyenQuyen_Modul2/src/ss10/Exercise/MyListTest.java
@@ -0,0 +1,25 @@
+package ss10.Exercise;
+
+import java.util.Arrays;
+
+public class MyListTest {
+    public static void main(String[] args) {
+    MyList<String> myList = new MyList<>(3);
+    myList.add(0,"1");
+    myList.add(1,"2");
+    myList.add(2,"3");
+    myList.add(3,"4");
+    myList.add(4,"5");
+    myList.add(5,"6");
+    myList.add(6,"7");
+        System.out.println(myList);
+        System.out.println(myList.isEmpty());
+        Object[] elements=(Object[]) myList.clone();
+        System.out.println(Arrays.toString(elements));
+        myList.remove(5);
+        System.out.println(myList);
+        myList.clear();
+        System.out.println(myList);
+
+    }
+}
