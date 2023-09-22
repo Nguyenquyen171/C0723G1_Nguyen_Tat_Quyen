e5db1c29ac0fd7fec0f3e1c15b36ab9148ad911f
Nguyenquyen171 <138420361+Nguyenquyen171@users.noreply.github.com>
2023-09-22 11:10:47 +0700

    Quyen nop bai tap

--- C0732G1_NguyenQuyen_Modul2/src/ss14/exercise/IllustratesInsertionSort.java
+++ C0732G1_NguyenQuyen_Modul2/src/ss14/exercise/IllustratesInsertionSort.java
@@ -6,11 +6,15 @@ public class IllustratesInsertionSort {
     public  static void sort(int list[]) {
         int n = list.length;
         for (int i = 1; i < n; ++i) {
+            System.out.println("Bước " + i + " gán x  = list[" + i + "] = " + list[i] + " pos= " + i);
             int index = list[i];
             int j = i - 1;
+
             while (j >= 0 && list[j] > index) {
+                System.out.println("Đổi chỗ cho list[" + index + "] và list[" + (index - 1) + "]");
                 list[j + 1] = list[j];
                 j = j - 1;
+
             }
             list[j + 1] = index;
             System.out.println("Loop " + i + Arrays.toString(list));
