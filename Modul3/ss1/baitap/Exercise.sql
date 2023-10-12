5f9f92338327207620e20c5a5a355529589d5a5d
Nguyenquyen171 <138420361+Nguyenquyen171@users.noreply.github.com>
2023-10-12 16:44:50 +0700

    Quyen nop bai tap

---
+++ Modul3/ss1/baitap/Exercise.sql
@@ -0,0 +1,31 @@
+
+drop database if exists student_management;
+create database if not exists student_management;
+use student_management;
+CREATE TABLE Students(
+id int primary key auto_increment,
+student_name VARCHAR(50),
+age int,
+country VARCHAR(50)
+);
+
+INSERT INTO Students(student_name, age, country)
+VALUES ('Nguyen Quyen', 22, 'Vietnam'), ('Nguyen Van A', 2, 'Vietnam'), ('Elon Musk',60,'Vietnam');
+
+CREATE TABLE Class(
+id int primary key auto_increment,
+class_name varchar(50)
+);
+
+INSERT INTO Class(class_name)
+VALUES ('C0623G1'), ('C0723G1'), ('C0623G1');
+
+CREATE TABLE Teachers(
+	id int primary key auto_increment,
+    teacher_name varchar(50),
+    age int,
+    country varchar(50)
+);
+
+INSERT INTO Teachers(teacher_name, age, country)
+VALUES ('Teacher A', 21, 'Vietnam'), ('Teacher B', 27, 'Vietnam'), ('Teacher C',40,'India');
