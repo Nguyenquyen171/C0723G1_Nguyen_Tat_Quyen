0987c7bc3bcc04ed929eb47a5b2b1bb9db294715
Nguyenquyen171 <138420361+Nguyenquyen171@users.noreply.github.com>
2023-10-15 17:03:28 +0700

    Quyen nop bai tap

--- Modul3/ss4/exercise.sql
+++ Modul3/ss4/exercise.sql
@@ -13,8 +13,10 @@ FROM
     subject s ON m.sub_id = s.sub_id
 GROUP BY s.sub_id;
 -- Hiển thị các thông tin sinh viên và điểm trung bình của mỗi sinh viên, xếp hạng theo thứ tự điểm giảm dần
-SELECT s.student_name, avg(mark) AS average_mark
-FROM student s
-JOIN mark m ON s.student_id = m.student_id
-GROUP BY s.student_id
-ORDER BY average_mark desc;
+SELECT 
+    s.*, AVG(mark) AS 'Diem thi trung binh'
+FROM
+    student s
+        JOIN
+    mark m ON s.student_Id = m.student_Id
+GROUP BY s.student_Id;
