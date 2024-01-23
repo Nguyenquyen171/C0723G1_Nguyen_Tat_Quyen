ab8fd42b09d3bb101f95a28bee736ff063d29f3d
Nguyenquyen171 <138420361+Nguyenquyen171@users.noreply.github.com>
2024-01-23 08:39:28 +0700

    Quyen nop bai tap

---
+++ Modul4/ss8/song/src/main/java/com/example/song/model/Song.java
@@ -0,0 +1,53 @@
+package com.example.song.model;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Song {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
+    private String singer;
+    private String typeMusic;
+
+    public Song() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSinger() {
+        return singer;
+    }
+
+    public void setSinger(String singer) {
+        this.singer = singer;
+    }
+
+    public String getTypeMusic() {
+        return typeMusic;
+    }
+
+    public void setTypeMusic(String typeMusic) {
+        this.typeMusic = typeMusic;
+    }
+}
